bbfa359dbd7ab8be044b9a7f90c4a9a30726071a
sheshan-hebron <sheshan@10qbit.com>
2026-09-17 14:51:12 +0530

    ecr updated for force delete

--- terraform/eks/ecr.tf
+++ terraform/eks/ecr.tf
@@ -1,14 +1,17 @@
 resource "aws_ecr_repository" "api" {
   name                 = "relay-api"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 }
 
 resource "aws_ecr_repository" "worker" {
   name                 = "relay-worker"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 }
 
 resource "aws_ecr_repository" "web" {
   name                 = "relay-web"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 }
